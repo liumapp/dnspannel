dda688e001cd4525718bb62040b2322e059b2822
liumei <liumeishengqi@huluwa.cc>
2017-05-11 15:57:11 +0800

    删除一条记录

--- src/js/delColumn.js
+++ src/js/delColumn.js
@@ -1,18 +1,21 @@
 $(function () {
     $('body').on('click' , '.lm-delete-btn' ,function () {
-        var tr = $(this).parent().parent();
-        $.ajax({
-            url:$.lmParam.deleteDnsRecordUrl,
-            method:"post",
-            data: {
-                id:$($(tr.children()[0]).children()[0]).val()
-            },
-            success: function (data) {
-                
-                tr.remove();
+        if (confirm('您确定要删除吗？')) {
+            var tr = $(this).parent().parent();
+            $.ajax({
+                url:$.lmParam.deleteDnsRecordUrl,
+                method:"post",
+                data: {
+                    id:$($(tr.children()[0]).children()[0]).val()
+                },
+                success: function (data) {
 
-            }
-        });
+                    tr.remove();
+
+                }
+            });
+        }
+
 
 
     });
